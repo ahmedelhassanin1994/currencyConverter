563b847d62e3353b3579c051708d2566b5712694
ahmed elhassanin <64086484+ahmedelhassanin1994@users.noreply.github.com>
2024-11-19 07:43:31 -0800

    MockLocalDataSource

--- test/features/home/data/data_source/local/local_data_source.dart
+++ test/features/home/data/data_source/local/local_data_source.dart
@@ -1,37 +1,41 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:mvvm_project/core/common/data/model/model_cache.dart';
 import 'package:mvvm_project/core/constant.dart';
 import 'package:mvvm_project/features/features_home/data/data_source/local_data_source.dart';
 import 'package:hive/hive.dart';
+import 'package:mvvm_project/features/features_home/data/responeses/model_country.dart';
 import 'local_data_source.mocks.dart';
 class MockLocalDataSource extends LocalDataSourceImplementer {
-  final MockBox<Model_Cache> mockBox;
+  final MockBox<ModelCountry> mockBox;
 
   MockLocalDataSource(this.mockBox);
 
   @override
-  Future<Box<Model_Cache>> Openbox() async => mockBox;
+  Future<Box<ModelCountry>> Openbox() async => mockBox;
 }
+
 @GenerateMocks([Box])
 void main() {
   late MockLocalDataSource localDataSource;
-  late MockBox<Model_Cache> mockBox;
+  late MockBox<ModelCountry> mockBox;
 
   setUp(() {
-    mockBox = MockBox<Model_Cache>();
+    mockBox = MockBox<ModelCountry>();
     localDataSource = MockLocalDataSource(mockBox);
   });
 
   group('AddCache', () {
     test('should add a Model_Cache to the box', () async {
-      final modelCache = Model_Cache("testKey","testValue");
-
+      final modelCache =  ModelCountry(name: 'Egypt',code: 'EG',flag: 'flag.png',currencyName: 'EGP',currencySymbol: 'EGP');
+      final tcountrys = [
+        ModelCountry(name: 'Egypt',code: 'EG',flag: 'flag.png',currencyName: 'EGP',currencySymbol: 'EGP'),
+        ModelCountry(name: 'Egypt',code: 'EG',flag: 'flag.png',currencyName: 'EGP',currencySymbol: 'EGP'),
+      ];
       // Stub the add method
       when(mockBox.add(modelCache)).thenAnswer((_) async => 1);
 
-      await localDataSource.AddCache(modelCache);
+      await localDataSource.AddCache(tcountrys);
 
       verify(mockBox.add(modelCache)).called(1);
     });
@@ -39,7 +43,7 @@ void main() {
 
   group('deleteLocal', () {
     test('should delete an item by title from the box', () async {
-      final modelCache = Model_Cache("testKey","testValue");
+      final modelCache =  ModelCountry(name: 'Egypt',code: 'EG',flag: 'flag.png',currencyName: 'EGP',currencySymbol: 'EGP');
       final deliveriesMap = {1: modelCache};
 
       when(mockBox.toMap()).thenReturn(deliveriesMap);
@@ -52,7 +56,7 @@ void main() {
 
   group('getCache', () {
     test('should return a Model_Cache with the specified key', () async {
-      final modelCache = Model_Cache(Constant.localKey,  "testValue");
+      final modelCache =  ModelCountry(name: 'Egypt',code: 'EG',flag: 'flag.png',currencyName: 'EGP',currencySymbol: 'EGP');
       when(mockBox.values).thenReturn([modelCache]);
 
       final result = await localDataSource.getCache();
@@ -60,7 +64,7 @@ void main() {
       expect(result, modelCache);
 
 
-      
+
     });
   });
 }
